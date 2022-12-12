aab107cc86deef70b8c437e67abece0ce345cd7a
VAZ <vazxxx@mail.ru>
2022-12-12 19:04:56 +0300

    тупит

---
+++ Задача 19/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Введите 5-ое число");
+int number = int.Parse(Console.ReadLine()!);
+
+int number = Math.Abs(number);
+if (10000 <= number && number <= 99999)
+{
+    int digit1 = number / 10000;
+    int digit2 = number / 1000 % 10;
+    int digit4 = number / 10 % 10;
+    int digit5 = number % 10;
+
+    if (digit1 == digit5 && digit2 == digit4)
+    {
+        Console.WriteLine("Число является полиндромом");
+    }
+else
+    {
+        Console.WriteLine("Number is a palindrom");
+    }
+}
+else
+{
+    Console.WriteLine("Number is not a palindrom");
+}
---
+++ Задача 21/Program.cs
@@ -0,0 +1,42 @@
+﻿
+// C# program to find
+// distance between
+// two points in 3 D.
+using System;
+ 
+class GFG
+{
+     
+// Function for
+// distance
+static void distance(float x1, float y1,
+                     float z1, float x2,
+                     float y2, float z2)
+{
+    double d = Math.Pow((Math.Pow(x2 - x1, 2) +
+                         Math.Pow(y2 - y1, 2) +
+                         Math.Pow(z2 - z1, 2) *
+                                   1.0), 0.5);
+    Console.WriteLine("Distance is \n" + d);
+    return;
+}
+ 
+// Driver code
+public static void Main()
+{
+    float x1 = 3;
+    float y1 = 6;
+    float z1 = 8;
+    float x2 = 2;
+    float y2 = 1;
+    float z2 = -7;
+     
+    // function call
+    // for distance
+    distance(x1, y1, z1,
+             x2, y2, z2);
+}
+}
+ 
+// This code is contributed
+// by chandan_jnu.
